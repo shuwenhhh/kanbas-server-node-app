9b756582e18a900ba2cbf18a06cc46bfd6b5fdf8
Shuwen Wang <shuwenwang97@gmail.com>
2023-12-09 23:12:05 -0500

    use env for db url

--- users/model.js
+++ users/model.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 import schema from "./schema.js";
 
-const PROD_URI = 'mongodb+srv://giuseppi:supersecretpassword@cluster0.eerap.mongodb.net/kanbas?retryWrites=true&w=majority'
-
 export const connectDb = () => {
     try {
-        const CONNECTION_STRING = process.env.DB_URL || PROD_URI;
-        console.log(CONNECTION_STRING);
-        mongoose.connect(CONNECTION_STRING);
+        const url = process.env.DB_URL;
+        console.log('connectDb:', { url });
+        if (url) {
+            mongoose.connect(url);
+        } else {
+            throw new Error('DB_URL not found');
+        }
     } catch (error) {
         console.error('connectDb failed', error);
     }
