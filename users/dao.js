7e2df10ef3213e3fe93e7f3079029135a6571fed
Shuwen Wang <shuwenwang97@gmail.com>
2023-12-09 22:01:24 -0500

    ss

--- users/dao.js
+++ users/dao.js
@@ -8,4 +8,5 @@ export const findUserByCredentials = (username, password) =>
   model.findOne({ username, password });
 export const updateUser = (userId, user) =>
   model.updateOne({ _id: userId }, { $set: user });
+
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
