845d9fc3825be9306a23627039e815c0eea218db
Shuwen Wang <shuwenwang97@gmail.com>
2023-12-09 22:50:21 -0500

    catch connect db

--- app.js
+++ app.js
@@ -6,17 +6,11 @@ import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
 import AssignmentRoutes from "./Assignments/routes.js";
 import 'dotenv/config';
-import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
+import { connectDb } from './users/model.js';
 
-
-
-//mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
-
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-console.log(CONNECTION_STRING);
-mongoose.connect(CONNECTION_STRING);
+connectDb();
 
 const app = express();
 console.log(process.env.FRONTEND_URL);
--- users/model.js
+++ users/model.js
@@ -1,4 +1,16 @@
 import mongoose from "mongoose";
 import schema from "./schema.js";
+
+export const connectDb = () => {
+    try {
+        const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+        console.log(CONNECTION_STRING);
+        mongoose.connect(CONNECTION_STRING);
+    } catch (error) {
+        console.error('connectDb failed', error);
+    }
+};
+
 const model = mongoose.model("users", schema);
+
 export default model;
